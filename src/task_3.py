b2e30c0b460832f481e674ff9693694d526f7f67
Dominik Malsch <dominik.malsch@plentymarkets.com>
2017-03-28 12:11:08 +0200

    fix

--- src/task_3.py
+++ src/task_3.py
@@ -27,41 +27,43 @@ def fussgaengerAmpel():
         if(LED_5_green == 0):
             ""
         else:
-            GPIO.output(LED_5_green)
+            GPIO.output(LED_5_green, True)
     elif(LED_3_green == 0):
         time.sleep(5)
-        GPIO.output(LED_3_green)
-        GPIO.output(LED_2_yellow)
+        GPIO.output(LED_3_green, True)
+        GPIO.output(LED_2_yellow, True)
         time.sleep(3)
-        GPIO.output(LED_2_yellow)
-        GPIO.output(LED_1_red)
-        GPIO.output(LED_5_green)
+        GPIO.output(LED_2_yellow, True)
+        GPIO.output(LED_1_red, True)
+        GPIO.output(LED_5_green, True)
         time.sleep(10)
-        GPIO.output(LED_5_green)
-        GPIO.output(LED_4_red)
+        GPIO.output(LED_5_green, True)
+        GPIO.output(LED_4_red, True)
         startAmpelSystem()
 
 
 def startAmpelSystem():
-    GPIO.output(LED_5_green)
-    GPIO.output(LED_2_yellow)
-    GPIO.output(LED_4_red)
+    GPIO.output(LED_5_green, True)
+    GPIO.output(LED_2_yellow, True)
+    GPIO.output(LED_4_red, True)
     time.sleep(3)
-    GPIO.output(LED_1_red)
-    GPIO.output(LED_2_yellow)
-    GPIO.output(LED_3_green)
+    GPIO.output(LED_1_red, True)
+    GPIO.output(LED_2_yellow, True)
+    GPIO.output(LED_3_green, True)
     time.sleep(15)
-    GPIO.output(LED_3_green)
-    GPIO.output(LED_2_yellow)
+    GPIO.output(LED_3_green, True)
+    GPIO.output(LED_2_yellow, True)
     time.sleep(3)
-    GPIO.output(LED_2_yellow)
-    GPIO.output(LED_1_red)
-    GPIO.Output(LED_4_red)
-    GPIO.output(LED_5_green)
+    GPIO.output(LED_2_yellow, True)
+    GPIO.output(LED_1_red, True)
+    GPIO.output(LED_4_red, True)
+    GPIO.output(LED_5_green, True)
+
 
 def end():
     global endTask
     endTask = 0
+
 
 def run():
     GPIO.setup(LED_1_red, GPIO.OUT)
@@ -73,9 +75,9 @@ def run():
     GPIO.setup(BUTTON_2, GPIO.IN)
     GPIO.setup(BUTTON_4, GPIO.IN)
 
-    GPIO.output(LED_1_red)
+    GPIO.output(LED_1_red, True)
     checkAmpelRot = True
-    GPIO.output(LED_4_red)
+    GPIO.output(LED_4_red, True)
     checkFuessgaengerAmpelRot = True
 
     GPIO.add_event_detect(BUTTON_1, GPIO.FALLING, callback=startAmpelSystem)
