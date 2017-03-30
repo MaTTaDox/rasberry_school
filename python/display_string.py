8b809cdba2caab0b1f2f10a3fc1937b067e6b644
Dominik Malsch <dominik.malsch@plentymarkets.com>
2017-03-30 14:46:50 +0200

    change matrix to lcd

--- python/display_string.py
+++ python/display_string.py
@@ -3,16 +3,18 @@
 
 import src.logic as logic
 import sys
-import max7219.led as led
+import lib.lcddriver as lcddriver
 
-matrix = led.matrix()
+lcd = lcddriver.lcd()
+lcd.lcd_clear()
 
 
 if __name__ == '__main__':  # Programmstart
 
     try:
         arg = sys.argv[1]
-        matrix.show_message(str(arg))
+        lcd.lcd_clear()
+        lcd.lcd_display_string(str(arg), 1)
         logic.destroy()
     except IndexError:
         print "Ungültige Auswahl"
