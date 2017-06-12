a9ca6b22b46c7c98dced9d30bf1b5bd4d3e3035b
Dominik Malsch <dominik.malsch@plentymarkets.com>
2017-06-12 09:55:41 +0200

    x

--- python/src/task_1.py
+++ python/src/task_1.py
@@ -8,11 +8,29 @@ import os
 import datetime  # GPIO-Bibliothek
 # oder "from RPi import GPIO"
 
+sensor = Adafruit_DHT.DHT11
+
 pin = 26
 delay = 5
 
+def loop():
+    while True:
+        luftfeuchte, temperatur = Adafruit_DHT.read_retry(sensor, pin)
+        messzeit = time.strftime("Am %d.%m.%Y um %H:%M:%S Uhr")
+        print '+-------------------------------------------------+'
+        print messzeit
+        print 'Temperatur: {0:0.1f}°C Luftfeuchtigkeit: {1:0.1f}%'.format(temperatur,luftfeuchte)
+        time.sleep(delay)
+
 
 def run():
-
+    print "Die Messung erfolgt alle %d Sekunden." % delay
+    
     while True:
-        ""
+        luftfeuchte, temperatur = Adafruit_DHT.read_retry(sensor, pin)
+        messzeit = time.strftime("Am %d.%m.%Y um %H:%M:%S Uhr")
+        print '+-------------------------------------------------+'
+        print messzeit
+        print 'Temperatur: {0:0.1f}°C Luftfeuchtigkeit: {1:0.1f}%'.format(temperatur,luftfeuchte)
+        time.sleep(delay)
+
