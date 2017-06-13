d9894169009e6e4b7f8ef435db06b2b9b41f5c0a
Dominik Malsch <dominik.malsch@plentymarkets.com>
2017-06-13 09:15:51 +0200

    create database

--- python/lib/mysql/init.py
+++ python/lib/mysql/init.py
@@ -13,10 +13,23 @@ def db():
 
         cur.execute("SHOW DATABASES LIKE 'readings'")
 
-        x = cur.fetchone()
+        result = cur.fetchone()
 
-        print vars(x)
+        if result is None:
+            cur.execute("CREATE DATABASE readings")
 
+            cur.execute("CREATE TABLE `locations` (`id` int(11) unsigned NOT NULL AUTO_INCREMENT, `identifer` varchar(200) NOT NULL DEFAULT '',PRIMARY KEY (`id`)) ENGINE=InnoDB DEFAULT CHARSET=utf8;")
+
+            cur.execute(
+                "CREATE TABLE `values` ("
+                "`id` int(11) unsigned NOT NULL AUTO_INCREMENT,"
+                "`location_id` int(10) unsigned NOT NULL,"
+                "`datetime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,"
+                "PRIMARY KEY (`id`),"
+                "KEY `location_id` (`location_id`),"
+                "CONSTRAINT `values_ibfk_1` FOREIGN KEY (`location_id`) REFERENCES `locations` (`id`) ON DELETE CASCADE ON UPDATE CASCADE"
+                ") ENGINE=InnoDB DEFAULT CHARSET=utf8;"
+            )
     except mdb.Error, e:
 
         print "Error %d: %s" % (e.args[0], e.args[1])
